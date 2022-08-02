b1a216d6a59d452e440fa936a7abf11972fcedd9
nguyenlemanhhung <github.nohssiw1905@gmail.com>
2022-08-02 09:10:31 +0700

    create app

---
+++ lib/drawer_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pet_app/config.dart';
+
+class DrawerScreen extends StatefulWidget {
+  const DrawerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DrawerScreen> createState() => _DrawerScreenState();
+}
+
+class _DrawerScreenState extends State<DrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: primaryGreen,
+    );
+  }
+}
